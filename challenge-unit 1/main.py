fdf49a3ec02a3766ae6afc8f02dc29b6ee8a5e04
VIGNESHS1075 <144253755+VIGNESHS1075@users.noreply.github.com>
2023-09-06 09:04:14 -0600

    Add files via upload

---
+++ challenge-unit 1/main.py
@@ -0,0 +1,10 @@
+num=int(input("\nEnter the number:"))
+
+deffactorial(num):
+ if num==0 or ==num1:
+  return 1
+else:
+  return num*factorial(num-1)
+
+result=factorial(num)
+print("the factorial of given number",num,"is",result)
